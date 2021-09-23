19276d8cb40cfc65ef0695f8fc357a5106cc24d3
erivanio <erivanio.vanin@gmail.com>
2021-09-23 12:32:06 -0300

    Configurando banco

--- src/vandog/settings.py
+++ src/vandog/settings.py
@@ -79,13 +79,9 @@ WSGI_APPLICATION = 'vandog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -125,3 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'public', 'static'))
+
+import django_heroku
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
